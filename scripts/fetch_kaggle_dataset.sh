4d0bdf376406e3399fc16a467805393ee8506e9f
Arun kumar G <g.aruniyer@gmail.com>
2021-06-19 01:48:26 +0530

    Update fetch_kaggle_dataset.sh

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-sudo apt install unzip
 kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/
-unzip house-prices-advanced-regression-techniques.zip
+ls packages/regression_model/regression_model/datasets/
